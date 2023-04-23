25039c43b2643258f0af054371b87776a6845d87
minseo205 <rlaalstj312@naver.com>
2023-04-24 00:37:53 +0900

    [0424] 02 - 새로운 구조와 할인 정책 적용

--- 02-스프링-핵심원리/core/src/main/java/hello/core/AppConfig.java
+++ 02-스프링-핵심원리/core/src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,9 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -13,7 +16,17 @@ public class AppConfig {
         return new MemberServiceImpl(new MemoryMemberRepository());
     }
 
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
     public OrderService orderService() {
         return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    }
+
+    // 할인 정책을 변경할 때, 애플리케이션의 구성 역할을 담당하는 AppConfig만 변경하면 됨 (공연의 기획자 역할)
+    public DiscountPolicy discountPolicy() {
+        // return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
